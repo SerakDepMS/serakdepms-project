3ece44017801a77f49c341f3305771165e8bf9bf
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2025-12-27 18:15:23 -0500

    feat: agregando spinner, toast y p condfirm dialogo a la aplicacion del sistema

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { NgxSpinnerComponent } from "ngx-spinner";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, BrowserAnimationsModule, NgxSpinnerComponent, ToastModule, ConfirmDialogModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
