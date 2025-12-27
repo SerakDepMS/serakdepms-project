b1b306bea3703b1572b55f9c65d19e88120c3eb7
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2025-12-27 16:06:36 -0500

    feat: asociados diferentes proveedores globales importantes para la app

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,17 +1,59 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withDebugTracing, withPreloading } from '@angular/router';
 import { routes } from './app.routes';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import { provideHttpClient, withFetch, withInterceptors, withJsonpSupport, withXsrfConfiguration } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
+  providers: [    
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    // Router con características avanzadas
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withPreloading(PreloadAllModules),
+      ...(isDevMode() ? [withDebugTracing()] : [])
+    ),
     providePrimeNG({
-        theme: {
-            preset: Aura
-        }
-    })
+      theme: {
+          preset: Aura
+      },
+      ripple: true,
+      inputStyle: 'outlined',
+      zIndex: {
+        modal: 1100,  
+        overlay: 1000,
+        menu: 1000,   
+        tooltip: 1100 
+      }
+    }),
+    /** Prime Ng Servicios Útiles */
+    MessageService,     
+    ConfirmationService,
+    DialogService,
+    // Animaciones (PrimeNG las necesita)
+    provideAnimations(),
+    provideAnimationsAsync(),
+    /** Http Client Avanzado */
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([/* tus interceptors */]),
+      withJsonpSupport(),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN'
+      })
+    ),
+    // Internationalización (i18n)
+    {
+      provide: 'DEFAULT_LANGUAGE',
+      useValue: 'es-ES'
+    },
   ]
 };
